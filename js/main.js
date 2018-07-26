d8d7d1126d1c70c27df21dea3e0a5a5e3e1d2eb4
Nils Layet <nils@epita.fr>
2018-07-26 12:45:46 +0200

    eat fruits

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ class Game {
     this.canvas = new Canvas(document.getElementById('canvas'));
 
     const { width, height } = this.canvas.getDimensions();
+
     this.snake = new Snake(rand(3, width - 6), rand(3, height - 6), 3);
+
+    const fp = this.randomFruitPosition();
+    this.fruit = new Fruit(fp.x, fp.y);
 
     document.addEventListener('keydown', e => {
       const dir = e.key.toLowerCase().slice('Arrow'.length);
@@ -22,14 +26,41 @@ class Game {
     this.redraw();
   }
 
+  randomFruitPosition() {
+    const { width, height } = this.canvas.getDimensions();
+    const snakeCells = this.snake.getCells();
+    const cells = [];
+
+    for (let i = 0; i < height; ++i) {
+      for (let j = 0; j < width; ++j)
+        cells.push({ x: j, y: i });
+    }
+
+    const availableCells = cells.filter(c => !snakeCells.find(sc => sc.x === c.x && sc.y === c.y));
+    const idx = rand(0, availableCells.length);
+
+    return availableCells[idx];
+  }
+
   redraw() {
     this.canvas.clear();
 
     this.snake.draw(this.canvas);
+    this.fruit.draw(this.canvas);
   }
 
-  frame() {
+  update() {
     this.snake.move();
+
+    const hp = this.snake.head.position;
+    const fp = this.fruit.position;
+
+    if (hp.x === fp.x && hp.y === fp.y) {
+      const nfp = this.randomFruitPosition();
+
+      this.snake.grow(2);
+      this.fruit = new Fruit(nfp.x, nfp.y);
+    }
   }
 
 }
@@ -124,6 +155,11 @@ class Snake extends Drawable {
 
     this.direction = 'left';
     this.nextDirection = null;
+    this.growLeft = 0;
+  }
+
+  grow(n) {
+    this.growLeft += n;
   }
 
   go(direction) {
@@ -154,6 +190,13 @@ class Snake extends Drawable {
       'down':   [ 0,  1],
     }[this.direction];
 
+    if (this.growLeft > 0) {
+      const p = body[body.length - 1].position;
+
+      this.body.push(new SnakeBody(p.x, p.y));
+      --this.growLeft;
+    }
+
     for (let i = body.length - 1; i > 0; --i)
       body[i].position = body[i - 1].position;
 
@@ -169,16 +212,27 @@ class Snake extends Drawable {
     this.body.forEach(b => b.draw(canvas));
   }
 
+  getCells() {
+    return [
+      this.head.position,
+      ...this.body.map(b => b.position),
+    ];
+  }
+
 }
 
-class Fruit extends Drawable {
+class Fruit extends Rectangle {
+
+  constructor(x, y) {
+    super(x, y, 'yellow');
+  }
 
 }
 
 const game = new Game();
 
 requestAnimationFrame(function frame() {
-  game.frame();
+  game.update();
   game.redraw();
-  setTimeout(() => requestAnimationFrame(frame), 1000);
+  setTimeout(() => requestAnimationFrame(frame), 100);
 });
